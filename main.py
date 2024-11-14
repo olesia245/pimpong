85035227ee4d7e58f604c455cc36cc44bca0964f
@olesia245 <o7315403@gmail.com>
2024-11-14 19:20:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ heght = 700
 window = display.set_mode((700,500))
 display.set_caption('Пин понг')
 clock = time.Clock()
+FPS = 60
 
 game = True
 
 class GameSprite(sprite.Sprite):
-    def __init__(self,player, player_x, player_y, speed, size_x, size_y):
+    def __init__(self,player, player_x, player_y, speed, size_x, size_y, speed_y):
         super().__init__()
         self.image = transform.scale(image.load(player), (size_x, size_y))
         self.rect = self.image.get_rect()
         self.speed = speed
+        self.speed_y = speed_y
         self.rect.x = player_x
         self.rect.y = player_y
     def reset(self):
@@ -35,16 +37,12 @@ class Player(GameSprite):
 
 class Enemy(GameSprite):
     def update(self):
-        self.rect.y += self.speed
-        global lost
-        if self.rect.y >= wids:
-            self.rect.y = 3
-            self.rect.x = 3
-            lost += 1
+        self.rect.y += self.speed_y
+        self.rect.x += self.speed
 
-player = Player('racket.png', 645, 100, 5 ,50, 150)
-player1 = Player('racket.png', 0, 200, 5 ,50, 150)
-
+player = Player('racket.png', 645, 100, 5 ,50, 150,0)
+player1 = Player('racket.png', 0, 200, 5 ,50, 150,0)
+enemy = Enemy('tenis_ball.png', 450, 0, 2, 80, 70,3)
 
 while game:
     window.fill((96, 152, 255))
@@ -52,9 +50,18 @@ while game:
         if e.type == QUIT:
             game = False
 
+    if sprite.collide_rect(player,enemy) or sprite.collide_rect(player1,enemy):
+        enemy.speed *= -1
+
+    if  enemy.rect.y < 0 or enemy.rect.y > wids - 80:
+        enemy.speed_y *= -1
+
     player.update_r()
     player.reset()
     player1.update_l()
     player1.reset()
+    enemy.update()
+    enemy.reset()
 
     display.update()
+    clock.tick(FPS)
